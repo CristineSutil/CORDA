2baebe385524a858647dd0740007d298cc1828b3
Cristine Sutil <43651678+CristineSutil@users.noreply.github.com>
2019-05-07 21:19:13 -0300

    Resposta02_01

---
+++ resptarefa02_01.java
@@ -0,0 +1,22 @@
+class IOUContract : Contract {
+    companion object {
+        @JvmStatic
+        val IOU_CONTRACT_ID = "net.corda.training.contract.IOUContract"
+    }
+
+    /**
+     * Add any commands required for this contract as classes within this interface.
+     * It is useful to encapsulate your commands inside an interface, so you can use the [requireSingleCommand]
+     * function to check for a number of commands which implement this interface.
+     */
+    interface Commands : CommandData {
+        class Issue: TypeOnlyCommandData(), CommandData
+    }
+
+    /**
+     * The contract code for the [IOUContract].
+     * The constraints are self documenting so don't require any additional explanation.
+     */
+    override fun verify(tx: LedgerTransaction) {
+        tx.commands.requireSingleCommand<Commands.Issue>()
+    }
